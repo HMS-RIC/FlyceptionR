98c15720d29db80d352d598c8e28d7adf0c64ee4
Ofer Mazor <omazor@users.noreply.github.com>
2020-01-30 10:20:47 -0500

    Improve ROI dF/F plots

--- R/measure_ROI.R
+++ R/measure_ROI.R
@@ -7,24 +7,26 @@
 #' measureROI()
 #'
 
-measureROI <- function(img, mask, output, goodfr=F){
+measureROI <- function(img, mask, output, goodfr=F, fps=100){
   ROI <- dipr::ssweep(img, mask, op="*")
   len <- length(which(mask==1))
   meanint <- colSums(ROI, dim=2)/len
   ROIF0 <- mean(meanint[1:5])
   deltaROIF <- meanint - ROIF0
   ROIdFF0 <- deltaROIF/ROIF0 * 100
-  dat <- data.frame(x=1:dim(img)[3], y=ROIdFF0)
-  p <- ggplot(data=dat, aes(x=x, y=y)) + geom_point() +
-    geom_smooth(method="loess", span = 0.05, level=0.95) +
+  dat <- data.frame(x=(1:dim(img)[3])/fps, y=ROIdFF0)
+  p <- ggplot(data=dat, aes(x=x, y=y)) + geom_point(size=0.25, alpha=0.5) +
+    geom_smooth(method="loess", span = 0.2, level=0.95) +
+    labs(x = "t (sec)", y = "dF/F (%)") +
     ylim(-5, 10)
   ggsave(filename = paste0(output, "_dFF0int_ROI.pdf"), width = 8, height = 8)
 
   if(goodfr!=F){
     ROIdFF0[!goodfr] <- NA
-    dat_goodfr <- data.frame(x=1:dim(img)[3], y=ROIdFF0)
-    p <- ggplot(data=dat, aes(x=x, y=y)) +
-      geom_smooth(method="loess", span = 0.4, level=0.95) +
+    dat_goodfr <- data.frame(x=(1:dim(img)[3])/fps, y=ROIdFF0)
+  p <- ggplot(data=dat, aes(x=x, y=y)) + geom_point(size=0.25, alpha=0.5) +
+      geom_smooth(method="loess", span = 0.2, level=0.95) +
+      labs(x = "t (sec)", y = "dF/F (%)") +
       ylim(-5, 10)
     ggsave(filename = paste0(output, "_dFF0int_ROI_goodfr.pdf"), width = 8, height = 8)
   }
